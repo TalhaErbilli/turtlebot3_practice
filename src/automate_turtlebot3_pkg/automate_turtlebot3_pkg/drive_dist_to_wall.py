fd7c74eebadbef48badc8fcf301e7f847a624307
TalhaErbilli <t.erbilli@live.nl>
2021-07-08 11:26:10 +0200

    organized upload

---
+++ src/automate_turtlebot3_pkg/automate_turtlebot3_pkg/drive_dist_to_wall.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+
+class AutomateTurtlebot(Node):
+    def __init__(self):
+        qos = QoSProfile(depth=1)
+        # TODO: change pub topic name -> Done
+        self.control_publisher = self.create_publisher(Twist, "/cmd_vel", qos)
+        # TODO: change sub topic ->
+        self.create_subscriber(LaserScan, "/scan", self.subscribe_callback)
+
+    def subscribe_callback(self, msg):
+        """
+        this function is executed every subscription.
+        """
+        try:
+            min_distance = self.calc_distance_to_wall(msg)
+            if min_distance < 0.1:
+                pub_msg = self.create_turnleft_msg()
+            else:
+                pub_msg = self.create_forward_msg()
+            self.control_publisher.publish(pub_msg)
+        except Exception as e:
+            print(e)
+
+        finally:
+            stop_msg = self.create_stop_msg()
+            self.control_publisher.publish(stop_msg)
+
+    def scan_callback(self, msg):
+        self.scan_ranges = msg.ranges
+        self.init_scan_state = True
+
+    def calc_distance_to_wall(self, LaserScan): #Float
+        # TODO implement get min distance from LaserScan -> Done
+        # NOTE: use range_min -> I did this similarly to the Twist
+        msg = LaserScan
+        msg.min_distance = min(self.scan_ranges)
+        return msg
+
+    def create_turnleft_msg(self, Twist): #Twist
+        # TODO: implement here -> tried some values, I will change this when I see how it moves
+        msg = Twist()
+        msg.linear.x = 0,2
+        msg.linear.y = -0,5
+        msg.linear.z = 0
+        msg.angular.x = 0
+        msg.angular.y = 0
+        msg.angular.z = 0
+        return msg
+
+    def create_forward_msg(self, Twist): #Twist
+        # TODO: implement here -> done
+        msg = Twist()
+        msg.linear.x = 1
+        msg.linear.y = 0
+        msg.linear.z = 0
+        msg.angular.x = 0
+        msg.angular.y = 0
+        msg.angular.z = 0
+        return msg
+
+    def create_stop_msg(self): #Twist
+        # TODO: implement here -> Done
+        msg = Twist()
+        msg.linear.x = 0
+        msg.linear.y = 0
+        msg.linear.z = 0
+        msg.angular.x = 0
+        msg.angular.y = 0
+        msg.angular.z = 0
+        return msg
+
+
+def main():
+    rclpy.init()
+    node = AutomateTurtlebot()
+    rclpy.spin(node)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/automate_turtlebot3_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'automate_turtlebot3_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='talha',
+    maintainer_email='t.erbilli@live.nl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'automate_turtlebot3_node = automate_turtlebot3_pkg.automate_turtlebot3_node:main',
+            'drive_dist_to_wall = automate_turtlebot3_pkg.drive_dist_to_wall:main',
+        ],
+    },
+)
