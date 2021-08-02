6c197caa7820442f2b8f93ef271a81e5c0777bdf
TalhaErbilli <t.erbilli@live.nl>
2021-08-02 16:34:53 +0200

    new 90 degrees turn

--- src/automate_turtlebot3_pkg/automate_turtlebot3_pkg/wall_follower_node.py
+++ src/automate_turtlebot3_pkg/automate_turtlebot3_pkg/wall_follower_node.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import math
+from typing import Pattern
 import numpy
 import sys
 import termios
 from numpy import float32, float_, left_shift, right_shift
+from numpy.lib.function_base import angle
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
@@ -53,41 +55,62 @@ class wall_follow(Node):
         self.start_time = None
 
 
-    def subscribe_callback(self, tf: TFMessage):
+    def subscribe_callback(self, min_dis: Float32MultiArray):
         """
         this function is executed every subscription.
         """
-        # float_array = min_dis.data
+        float_array = min_dis.data
         # print("\n \n \n --------------------------------------- \n \n"
         # "\n min distance_front:  ", float_array[0],"\n \n"
         # " min_distance_left:  ", float_array[1],"\n \n"
         # " min_distance_right:  ", float_array[2],
         # "\n \n \n --------------------------------------- \n \n \n \n \n")
 
-        #self.control_publisher.publish()
+        self.control_publisher.publish(self.create_forward_msg())
+        print("drive_fwd", float_array[0])
 
-        # self.control_publisher.publish(self.create_forward_msg())
-        # drive forward for 3 seconds
-        # self.control_publisher.publish(self.create_stop_msg())
-        # also, is there a good way to not execute the above if it already did it once?
+        if float_array[0] < 0.5:
+            self.control_publisher.publish(self.create_stop_msg())
+            print("stopped robot for a sec")
+            
+        #after stopping robot, let's turn a perfect 90 degrees.
+        self.control_publisher.publish(self.create_turnright_msg())
+        print("turning robot _R_:  ")
+        #during the turn, we need to check the base_link transform
+        #and compare this to the transformation we need.
+        old_yaw = self.tf_timer_callback()    
+        new_yaw = self.calculate_new_yaw()
+
+        if new_yaw == old_yaw:
+            self.control_publisher.publish(self.create_stop_msg())
+            print("stopped robot for a sec 2")          
+
+
+
+        
+        
+        # #the code below is to practice simple commands.
+        # float_array = min_dis.data
+        # #drive forward until front sensor(float_array[0]< x)
         # self.control_publisher.publish(self.create_forward_msg())
         # print("drive_fwd")
         # if float_array[0] < 0.5:
         #     print("turn_left", float_array[1])
         #     self.control_publisher.publish(self.create_turnleft_msg())
-        #     if 0.2 < float_array[2] > 0.4:
+
+        # #Now our robot is facing left, so the right sensor should face the wall, that's why we're looking at float_array[2].
+        #     if float_array[2] < 0.3:
         #         print("turn_right:  ", float_array[2])
         #         self.control_publisher.publish(self.create_turnright_msg())
-        #     else:
-        #         print("turn_left_2:  ", float_array[1])
+        # #If we are too close to the wall, we should turn back right a little bit, but not too far. 
+        #     elif float_array[2] > 0.6:
+        #         print("turn_left_2:  ", float_array[2])
         #         self.control_publisher.publish(self.create_turnleft_msg())
-        # if float_array[0] < 0.4:
-        #     print("turn left")
-        #     pub_msg = self.create_turnleft_msg()
-        # else:
-        #     print("drive_fwd")
-        #     pub_msg = self.create_forward_msg()
-        # self.control_publisher.publish(pub_msg)
+        # #in any other case we must be within acceptable ranges, so we can drive straight.
+        #     else:
+        #         print("drive_fwd_2:  ", float_array[0])
+        #         self.control_publisher.publish(self.create_forward_msg())
+
 
     def tf_timer_callback(self, ):
         transform = self.tf_utils.lookup_transform(
@@ -101,22 +124,39 @@ class wall_follow(Node):
         #From the Euler_from quaternion function:
         Euler = self.euler_from_quaternion(self.turtlbot_state)
         yaw = Euler[2]
-        #print(yaw)
+        # print("yaw:  ", yaw)
+        return yaw
 
 
     def control_timer_callback(self, ):
         if self.start_time is None:
-            self.start_time = self.get_clock().now().nanoseconds()
-            print(self.start_time)
+            self.start_time = self.get_clock().now().nanoseconds
+            #print(self.start_time)
 
         #TODO: something is wrong with this line below, try to fix it (added .seconds_nanoseconds)
-        if (self.get_clock().now().nanoseconds() - self.start_time) < 3 *10**9:
+        if (self.get_clock().now().nanoseconds - self.start_time) < 3 *10**9:
             # forward
             control_msg = self.create_forward_msg()
         else:
             # stop
             control_msg = self.create_stop_msg()
-        self.control_publisher.publish(control_msg)
+        #self.control_publisher.publish(control_msg)
+
+    def calculate_new_yaw(self):
+        old_yaw = self.tf_timer_callback()
+        #print("old yaw:  ", old_yaw)
+        angle_of_turn = 90.0
+        #we are adding the turn to the yaw
+        new_yaw = old_yaw + angle_of_turn
+        #print("new yaw:  ", new_yaw)
+
+        return new_yaw
+
+        # self.control_publisher.publish(self.create_turnright_msg())
+        # print("turn_right")
+        # if old_yaw == new_yaw:
+        #     self.control_publisher.publish(self.create_stop_msg())
+        #     print("turned ", angle_of_turn, "degrees")
 
 
     def create_turnright_msg(self) -> Twist:
@@ -126,7 +166,7 @@ class wall_follow(Node):
         turn_right.linear.z = 0.0
         turn_right.angular.x = 0.0
         turn_right.angular.y = 0.0
-        turn_right.angular.z = -0.6
+        turn_right.angular.z = -0.2
         return turn_right
 
     def create_turnleft_msg(self) -> Twist:
@@ -136,7 +176,7 @@ class wall_follow(Node):
         turn_left.linear.z = 0.0
         turn_left.angular.x = 0.0
         turn_left.angular.y = 0.0
-        turn_left.angular.z = 0.6
+        turn_left.angular.z = 0.2
         return turn_left
 
     def create_forward_msg(self) -> Twist:
